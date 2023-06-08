7d560030a97c6e6741f1b1e51738f9ee36ebc861
soukainaelansari <120578814+soukainaelansari@users.noreply.github.com>
2023-06-08 23:04:12 +0100

    first commit

--- src/components/Why.js
+++ src/components/Why.js
@@ -1,6 +1,9 @@
-import meter1 from "../assets/img/image_2023-06-07_174831907-removebg-preview.png";
-import meter2 from "../assets/img/User-friendly.png";
-import meter3 from "../assets/img/image_2023-06-07_174831907-removebg-preview.png";
+import meter1 from "../assets/img/upload.png";
+import meter2 from "../assets/img/verified-account.png";
+import meter3 from "../assets/img/image_2023-06-08_212726600-removebg-preview.png";
+import meter4 from "../assets/img/1584849.png";
+import meter5 from "../assets/img/9876324.png";
+import meter6 from "../assets/img/hands.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
@@ -37,7 +40,7 @@ export const Why = () => {
                         <h2>Why TeethSeg</h2>
                         <p>Teeth Segmentation Service by TeethSeg Highest Degree of Efficiency and Accuracy.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
+                            <div className="item1">
                                 <img src={meter1} alt="Image" />
                                 <h5>Web-based</h5>
                                 <p>Obtain a smooth and ultra-fast performance with our solution that doesn't rely on cloud computing! For best outcomes, access it through Google Chrome on a computer equipped with a GPU..</p>
@@ -47,13 +50,25 @@ export const Why = () => {
                                 <h5>User-friendly</h5>
                                 <p>Discover the cutting-edge of Teeth segmentation through our radiologist-designed, user-friendly tool. Enjoy streamlined workflows and enhanced precision thanks to our gaming-inspired controls and radiology expertise.</p>
                             </div>
-                            <div className="item">
+                            <div className="item1">
                                 <img src={meter3} alt="Image" />
-                                <h5>3D avantage</h5>
+                                <h5>3D</h5>
+                                <p>TeethSeg offers advanced 3D dental segmentation functionalities. This allows users to obtain accurate and detailed results for their three-dimensional dental models.</p>
                             </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                            <div className="item1">
+                                <img src={meter4} alt="Image" />
+                                <h5>Time-saving</h5>
+                                <p>With TeethSeg, you can save valuable time in your dental workflow. Our automated end-to-end 3D teeth segmentation approach eliminates the need for manual segmentation, which can be time-consuming and prone to errors.</p>
+                            </div>
+                            <div className="item1">
+                                <img src={meter5} alt="Image" />
+                                <h5>Continuous Improvement</h5>
+                                <p>At TeethSeg, we are committed to continuous improvement and innovation. We regularly update our algorithms and features to ensure the highest accuracy and efficiency in teeth segmentation. By choosing TeethSeg, you benefit from ongoing advancements in the field, keeping you at the forefront of dental technology.</p>
+                            </div>
+                            <div className="item1">
+                                <img src={meter6} alt="Image" />
+                                <h5>Exceptional Support</h5>
+                                <p>We pride ourselves on providing exceptional customer support. Our team is dedicated to assisting you throughout your TeethSeg experience, addressing any questions or concerns you may have. We value your feedback and strive to deliver a seamless and satisfactory user experience.</p>
                             </div>
                         </Carousel>
                     </div>
